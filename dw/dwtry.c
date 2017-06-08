e23cbde4e6557004253c109885bd9833e5a08d9a
Chuck Cranor <chuck@ece.cmu.edu>
2017-06-08 16:14:48 -0400

    try

--- dw/dwtry.c
+++ dw/dwtry.c
@@ -1,6 +1,7 @@
 /* "module load datawarp" */
 /* DW mount point on compute notes: $DW_JOB_STRIPED */
 
+#include <err.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -27,27 +28,22 @@ int main(int argc, char **argv) {
     int size, width, start;
     printf("datawarp test program\n");
     dw = getenv("DW_JOB_STRIPED");
-    if (!dw) {
-        printf("datawarp not configured\n");
-        exit(1);
-    }
+    if (!dw) errx(1, "datawarp not configured");
+
+    rv = dw_get_stripe_configuration(0, &size, &width, &start);
+    if (rv >= 0) 
+        errx(1, "unexpected rv %d from dw_get_stripe_configuration", rv);
+    printf("got %s on an invalid stripe cfg call\n", strerror(-rv));
 
     printf("DW_JOB_STRIPED = %s\n", dw);
     fd = open(dw, O_RDONLY);
-    if (fd < 0) {
-        perror("open dw");
-        exit(1);
-    }
-    printf("attempt on stdin: %d\n", 
-         dw_get_stripe_configuration(0, &size, &width, &start));
+    if (fd < 0) err(1, "can't open DW_JOB_STRIPED dir");
+
     rv = dw_get_stripe_configuration(fd, &size, &width, &start);
-    if (rv < 0) {
-        fprintf(stderr, "dw_get_stripe_configuration failed %d\n", rv);
-        exit(1);
-    } else {
-        printf("top-level stripe cfg: size=%d, width=%d, start=%d\n",
-               size, width, start);
-    }
+    if (rv < 0) 
+        errx(1, "get stripe cfg err: %s", strerror(-rv);
+    printf("top-level stripe cfg: size=%d, width=%d, start=%d\n",
+           size, width, start);
 
     exit(0);
 }
